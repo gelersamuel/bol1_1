3bec8b9aee1bed8b9938f937da876d5dc7498248
geler <gparramieses@danielcastelao.org>
2019-10-08 09:35:39 +0200

    first commit

---
+++ bol1_1/Bol1_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol1_1;
+
+/**
+ *
+ * @author gparramieses
+ */
+public class Bol1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int b = 4,h =3,a;
+        a = b*h/2;
+      System.out.println("A area de triangulo e:"+a);
+    }
+}      
+      
